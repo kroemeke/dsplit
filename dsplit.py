5f9456b278802ad5ff7389d728434c59fd5043b5
Marek Kroemeke <kroemeke+github@gmail.com>
2021-07-16 23:27:48 +0100

    Updated to python3 + fixes
    Fixes for python3
    Fixed arg parsing for bdr option
    Better formatting
    Changed bdr size to dit on my bdrs
    
    Signed-off-by: Marek Kroemeke <kroemeke+github@gmail.com>

--- dsplit.py
+++ dsplit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Created   : Sun 26 Jul 2009 12:12:40 AM PDT
 # Modified  : Sun 26 Jul 2009 12:12:40 AM PDT
 # Author    : Gautam Iyer <gi1242@users.sourceforge.net>
@@ -14,11 +14,12 @@ from getopt import getopt, GetoptError
 
 # Functions {{{1
 def die(msg):
-    print ER+msg+RE
+    print(ER + msg + RE)
     sys.exit(1)
 
+
 def usage():
-    print """\
+    print("""\
 dsplit [options] src dst
 
 Splits src into CD/DVD sized sub-directories. Output is in vol-%2d subdirs of
@@ -36,140 +37,152 @@ OPTIONS
     --for-bdr   Set volume size for BDR (23800MB)
    
     -v		Verbose (print all subdirectories included in volumes)
-    -V		Very verbose (print all files included too)"""
+    -V		Very verbose (print all files included too)""")
     sys.exit(0)
 
-def newvolume( output, volume, volsize, thisvolfiles, thisvoldirs ):
+
+def newvolume(output, volume, volsize, thisvolfiles, thisvoldirs):
     if volume != 0:
-	stdout.write( ( BD+"Total: %.2f MB (%d files, %d dirs)\n"+RE ) % \
-		( volsize / MB, thisvolfiles, thisvoldirs ) )
+        stdout.write(
+            (BD + "Total: %.2f MB (%d files, %d dirs)\n" + RE)
+            % (volsize / MB, thisvolfiles, thisvoldirs)
+        )
 
     if volume >= 0:
-	volume += 1
-	volsize = 0
+        volume += 1
+        volsize = 0
 
-	stdout.write( (BD + "Volume %02d:" + RE +"%s") % \
-		(volume, verbose and "\n" or "" ) )
-	voldir = path.join( output, "vol-%02d" % volume )
+        stdout.write(
+            (BD + "Volume %02d:" + RE + "%s") % (volume, verbose and "\n" or "")
+        )
+        voldir = path.join(output, "vol-%02d" % volume)
 
-	return (voldir, volume, volsize)
+        return (voldir, volume, volsize)
 
-def addfile( targetdir, root, f ):
+
+def addfile(targetdir, root, f):
     if veryverbose:
-	stdout.write( (IT + "%s " + RE) % (f) )
-    os.link( path.join( root, f), path.join( targetdir, f ) )
+        stdout.write((IT + "%s " + RE) % (f))
+    os.link(path.join(root, f), path.join(targetdir, f))
 
-#}}}
+
+# }}}
 
 # Code starts here
 global BD, UL, IT, ER, RE
 
-BD="\033[36m"	# Cyan
-UL="\033[32m"	# Green
-IT="\033[33m"	# Yellow
-ER="\033[31m"	# Red
-RE="\033[0m"	# Normal
+BD = "\033[36m"  # Cyan
+UL = "\033[32m"  # Green
+IT = "\033[33m"  # Yellow
+ER = "\033[31m"  # Red
+RE = "\033[0m"  # Normal
 
 global verbose, veryverbose
-verbose=False
-veryverbose=False
+verbose = False
+veryverbose = False
 
-MB=1024*1024.0
-maxSize=4400 * MB
-cdsize=680*MB
-dvdsize=4400*MB
-dvd2size=8080*MB
-bdrsize=23800*MB
+MB = 1024 * 1024.0
+maxSize = 4400 * MB
+cdsize = 680 * MB
+dvdsize = 4400 * MB
+dvd2size = 8080 * MB
+bdrsize = 23000 * MB
 
 try:
-    (popts, argv) = getopt( argv[1:], 's:vVh',
-	    ['volume-size=', 'for-cd', 'for-dvd', 'for-dvd2'] )
-except GetoptError, opt:
-    die(str(opt));
+    (popts, argv) = getopt(
+        argv[1:], "s:vVh", ["volume-size=", "for-cd", "for-dvd", "for-bdr"]
+    )
+except GetoptError as opt:
+    die(str(opt))
 
 for (opt, value) in popts:
-    if opt == '-s' or opt == '--volume-size':
-	maxSize = float(value)*MB;
-    elif opt == '--for-cd':
-	maxSize = cdsize
-    elif opt == '--for-dvd':
-	maxSize = dvdsize
-    elif opt == '--for-dvd2':
-	maxSize = dvd2size
-    elif opt == '--for-bdr':
+    if opt == "-s" or opt == "--volume-size":
+        maxSize = float(value) * MB
+    elif opt == "--for-cd":
+        maxSize = cdsize
+    elif opt == "--for-dvd":
+        maxSize = dvdsize
+    elif opt == "--for-dvd2":
+        maxSize = dvd2size
+    elif opt == "--for-bdr":
         maxSize = bdrsize
-    elif opt == '-v':
-	verbose=True
-    elif opt == '-V':
-	verbose = True
-	veryverbose = True
-    elif opt == '-h':
-	usage()
-#end for (opt,value)
+    elif opt == "-v":
+        verbose = True
+    elif opt == "-V":
+        verbose = True
+        veryverbose = True
+    elif opt == "-h":
+        usage()
+# end for (opt,value)
 
 if len(argv) < 2:
-    die( "USAGE: dsplit [options] src dst. (Use -h for help)" )
+    die("USAGE: dsplit [options] src dst. (Use -h for help)")
 else:
     input = argv[0]
     output = argv[1]
 
-if path.exists( output ):
-    die( "%s already exists." % output )
+if path.exists(output):
+    die("%s already exists." % output)
 
-(voldir, volume, volsize) = newvolume( output, 0, 0, 0, 0 )
+(voldir, volume, volsize) = newvolume(output, 0, 0, 0, 0)
 thisvolfiles = 0
 thisvoldirs = 0
 for root, dirs, files in os.walk(input, True):
     if verbose:
-	stdout.write( "  %s: " % (root) )
+        stdout.write("  %s: " % (root))
 
-    thisdirfiles=0
-    thisdirsize=0
+    thisdirfiles = 0
+    thisdirsize = 0
 
-    if root.find( input ) != 0:
-	die( "%s isn't a subdir of %s" % (root, input) )
+    if root.find(input) != 0:
+        die("%s isn't a subdir of %s" % (root, input))
 
-    targetdir = path.join( voldir, root.replace( input, "", 1 ) )
+    targetdir = path.join(voldir, root.replace(input, "", 1))
+    print("target : " + targetdir )
 
     for f in files:
-	# File size without following symlinks.
-	s = os.lstat( path.join( root, f ) ).st_size
+        # File size without following symlinks.
+        s = os.lstat(path.join(root, f)).st_size
 
-	if s > maxSize:
-	    stdout.write( (ER + "%s (%.2f MB) " + RE) % (f, s / MB) )
-	    continue
+        if s > maxSize:
+            stdout.write((ER + "%s (%.2f MB) " + RE) % (f, s / MB))
+            continue
 
-	elif s + volsize > maxSize:
-	    if verbose:
-		stdout.write( ("%d files ("+UL+"%.2f MB"+RE+").\n") % \
-			(thisdirfiles, thisdirsize/MB) )
-	    thisdirfiles = 0
-	    thisdirsize = 0
+        elif s + volsize > maxSize:
+            if verbose:
+                stdout.write(
+                    ("%d files (" + UL + "%.2f MB" + RE + ").\n")
+                    % (thisdirfiles, thisdirsize / MB)
+                )
+            thisdirfiles = 0
+            thisdirsize = 0
 
-	    (voldir, volume, volsize) = \
-		    newvolume( output, volume, volsize, thisvolfiles, \
-			thisvoldirs )
-	    targetdir = path.join( voldir, root.replace( input, "", 1 ) )
-	    thisvolfiles = 0
-	    thisvoldirs = 0
+            (voldir, volume, volsize) = newvolume(
+                output, volume, volsize, thisvolfiles, thisvoldirs
+            )
+            targetdir = path.join(voldir, root.replace(input, "", 1))
+            thisvolfiles = 0
+            thisvoldirs = 0
 
-	    if verbose:
-		stdout.write( "  %s: " % (root) )
+            if verbose:
+                stdout.write("  %s: " % (root))
 
-	if thisdirfiles == 0:
-	    thisvoldirs += 1
-	    if not path.isdir( targetdir ):
-		os.makedirs( targetdir )
-		
-	addfile( targetdir, root, f )
-	thisvolfiles += 1
-	thisdirfiles += 1
-	thisdirsize += s
-	volsize += s
-    #end for f
+        if thisdirfiles == 0:
+            thisvoldirs += 1
+            if not path.isdir(targetdir):
+                os.makedirs(targetdir)
+
+        addfile(targetdir, root, f)
+        thisvolfiles += 1
+        thisdirfiles += 1
+        thisdirsize += s
+        volsize += s
+        # end for f
     if verbose:
-	stdout.write( ("%d files ("+UL+"%.2f MB"+RE+").\n") % \
-		(thisdirfiles, thisdirsize/MB) )
+        stdout.write(
+            ("%d files (" + UL + "%.2f MB" + RE + ").\n")
+            % (thisdirfiles, thisdirsize / MB)
+        )
 
-#end for (...) in os.walk
-newvolume( output, -1, volsize, thisvolfiles, thisvoldirs )
+# end for (...) in os.walk
+newvolume(output, -1, volsize, thisvolfiles, thisvoldirs)
